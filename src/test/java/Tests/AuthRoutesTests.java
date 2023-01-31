f6779b66bca63975c8fb0b848b3d7a78a57c698e
Dragan Radojevic <995dragan995@gmail.com>
2023-01-31 21:50:45 +0100

    Testing Visiting Admin Cities and Admin Users When Not Logged in

--- src/test/java/Tests/AuthRoutesTests.java
+++ src/test/java/Tests/AuthRoutesTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 public class AuthRoutesTests extends BaseTest{
 
+
     @Test
     public void visitHomeWhenNotLoggedInTest() {
         driver.get("https://vue-demo.daniel-avellaneda.com/home");
@@ -14,6 +15,18 @@ public class AuthRoutesTests extends BaseTest{
     @Test
     public void visitProfileWhenNotLoggedInTest() {
         driver.get("https://vue-demo.daniel-avellaneda.com/profile");
+        Assert.assertTrue(driver.getCurrentUrl().endsWith("/login"));
+    }
+
+    @Test
+    public void visitAdminCitiesWhenNotLoggedInTest() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/cities");
+        Assert.assertTrue(driver.getCurrentUrl().endsWith("/login"));
+    }
+
+    @Test
+    public void visitAdminUsersWhenNotLoggedInTest() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/users");
         Assert.assertTrue(driver.getCurrentUrl().endsWith("/login"));
     }
 
