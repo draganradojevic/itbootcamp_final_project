b56546194ea26bfd899bdd082eebca0318243e3a
Dragan Radojevic <995dragan995@gmail.com>
2023-01-31 18:59:42 +0100

    Testing login as non-existing user

--- src/test/java/Tests/LoginTests.java
+++ src/test/java/Tests/LoginTests.java
@@ -2,6 +2,9 @@ package Tests;
 
 import Pages.HomePage;
 import Pages.LoginPage;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -37,6 +40,18 @@ public class LoginTests extends BaseTest{
 
         Assert.assertEquals(emailType, "email");
         Assert.assertEquals(passwordType, "password");
+    }
+
+    @Test
+    public void nonexistingUserTest() {
+        Faker faker = new Faker();
+        loginPage.login(faker.internet().emailAddress(), faker.internet().password());
+
+        WebElement error = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div/div[1]/ul/li"));
+        String errorMsg = error.getText();
+
+        Assert.assertEquals(errorMsg, "User does not exists");
+        Assert.assertTrue(driver.getCurrentUrl().endsWith("/login"));
     }
 
 
