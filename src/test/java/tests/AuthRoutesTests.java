5ea50d63606832f6aad0ea80b9d10ffcd74f975c
Dragan Radojevic <995dragan995@gmail.com>
2023-02-07 12:36:41 +0100

    Changed part of url with constant BASE_URL in AuthRoutesTests

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -7,28 +7,28 @@ public class AuthRoutesTests extends BaseTest{
 
     @Test
     public void visitHomeWhenNotLoggedInTest() {
-        driver.navigate().to("https://vue-demo.daniel-avellaneda.com/home");
+        driver.navigate().to(BASE_URL + "/home");
 
         Assert.assertTrue(driver.getCurrentUrl().endsWith("/login"));
     }
 
     @Test
     public void visitProfileWhenNotLoggedInTest() {
-        driver.navigate().to("https://vue-demo.daniel-avellaneda.com/profile");
+        driver.navigate().to(BASE_URL + "/profile");
 
         Assert.assertTrue(driver.getCurrentUrl().endsWith("/login"));
     }
 
     @Test
     public void visitAdminCitiesWhenNotLoggedInTest() {
-        driver.navigate().to("https://vue-demo.daniel-avellaneda.com/admin/cities");
+        driver.navigate().to(BASE_URL + "/admin/cities");
 
         Assert.assertTrue(driver.getCurrentUrl().endsWith("/login"));
     }
 
     @Test
     public void visitAdminUsersWhenNotLoggedInTest() {
-        driver.navigate().to("https://vue-demo.daniel-avellaneda.com/admin/users");
+        driver.navigate().to(BASE_URL + "/admin/users");
 
         Assert.assertTrue(driver.getCurrentUrl().endsWith("/login"));
     }
